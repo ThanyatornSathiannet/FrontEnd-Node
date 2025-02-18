de823463fc9b95f2e30c40dc63e70eb25d3c95ba
ThanyatornSathiannet <s6606021610079@email.kmutnb.ac.th>
2025-02-18 09:01:36 +0700

    edit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
-var bodyParser = require('body-parser');
 const path = require('path');
+var bodyParser = require('body-parser');
+
 
 const base_url = "http://localhost:3000";
 // const base_url =
 
 //app.use(express.static(__dirname + '/public/views'));
-app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "/public/css/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
